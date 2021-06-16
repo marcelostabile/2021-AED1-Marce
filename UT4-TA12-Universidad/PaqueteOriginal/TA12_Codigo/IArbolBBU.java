44dc44c192992f4fb56973b437c1ed04886d3385
Marcelo Stabile <marcelo.stabile@correo.ucu.edu.uy>
2021-06-16 15:13:37 -0300

    .

--- UT4-TA12-Universidad/PaqueteOriginal/TA12_Codigo/IArbolBBU.java
+++
@@ -1,8 +0,0 @@
-
-public interface IArbolBBU {
-
-      
-       public void armarIndicesCarrreras (Lista<Carrera> carreras);
-	
-}
-
--- X-UT3-TODO-Listas/src/tdas/IParOrdenado.java
+++
@@ -1,9 +0,0 @@
-package tdas;
-
-public interface IParOrdenado<K, V> {
-
-    public K getClave();
-
-    public V getValor();
-
-}
