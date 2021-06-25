03e36a9b0e82abf18ee01aaf64bb22b92554b7ca
Marcelo Stabile <marcelo.stabile@correo.ucu.edu.uy>
2021-06-24 22:38:21 -0300

    .

---
+++ UT0-2021/UT0_TA2/Proyecto Hola Mundo/src/HolaMundo.java
@@ -0,0 +1,10 @@
+import jdk.internal.util.xml.impl.Input;
+
+public class HolaMundo{
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola, " + args);
+        System.out.println(args.length);
+    }
+}
