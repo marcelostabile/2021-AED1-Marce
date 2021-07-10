77f4404c99289980d1881672c45d5da9ac05ac7b
Marcelo Stabile <marcelo.stabile@correo.ucu.edu.uy>
2021-07-10 20:11:24 -0300

    .

--- Unidad 5/UT5 Tareas - Soluciones/UT5-TA11-Marce/src/com/ucudal/tdas/ICalculadorMatricesOptimo.java
+++
@@ -1,9 +0,0 @@
-package com.ucudal.tdas;
-
-public interface ICalculadorMatricesOptimo {
-
-    public void armarArbolBinario(int i, int j, IElementoAB[] elementos, IArbolBB elArbolBB);
-
-    public void encontrarOptimo(int cantElem, int[] FrecExito, int[] FrecNoExito);
-
-}
